225f382f4f02032577295ad4ebcb86145c3bb01e
walterfb <wbrisken@gmail.com>
2008-07-23 13:11:24 +0000

    Remove unneeded error messages
    git-svn-id: https://svn.atnf.csiro.au/difx@658 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- common/mark5access/branches/NRAO-DiFX-1.1/mark5access/mark5_format_mark4.c
+++ common/mark5access/branches/NRAO-DiFX-1.1/mark5access/mark5_format_mark4.c
@@ -6821,7 +6821,6 @@ static int mark5_format_mark4_init(struct mark5_stream *ms)
 	{
 		if(ms->datawindowsize < ms->framebytes)
 		{
-		        fprintf(stderr, "mark5_format_mark4_init: datawindowsize < framebytes\n");
 			return -1;
 		}
 
@@ -6832,7 +6831,6 @@ static int mark5_format_mark4_init(struct mark5_stream *ms)
 			f->ntrack, tol);
 		if(ms->frameoffset < 0)
 		{
-		        fprintf(stderr, "mark5_format_mark4_init: frameoffset<0\n");
 			return -1;
 		}
 

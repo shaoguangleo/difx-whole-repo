bff84b6744c57aabc6dbaa3f8af497db2670b87a
adamdeller <adeller@swin.edu.au>
2008-11-06 01:01:18 +0000

    Added empty definitions of difxtime.h and difxtime.cpp
    git-svn-id: https://svn.atnf.csiro.au/difx@858 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ mpifxcorr/branches/adeller-time/src/difxtime.h
@@ -0,0 +1,22 @@
+/***************************************************************************
+*   Copyright (C) 2008 by James M Anderson <anderson@mpifr-bonn.mpg.de>   *
+*   Adapted from FxTime to DifxTime by Adam Deller (2008)                 *
+*                                                                         *
+*   This program is free for non-commercial use: see the license file     *
+*   at http://astronomy.swin.edu.au:~adeller/software/difx/ for more      *
+*   details.                                                              *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+***************************************************************************/
+//===========================================================================
+// SVN properties (DO NOT CHANGE)
+//
+// $Id: difxtime.h 857 2008-11-06 00:39:49Z AdamDeller $
+// $HeadURL: https://svn.atnf.csiro.au/difx/mpifxcorr/branches/adeller-time/src/difxtime.h $
+// $LastChangedRevision: 857 $
+// $Author: AdamDeller $
+// $LastChangedDate: 2008-11-05 17:39:49 -0700 (Wed, 05 Nov 2008) $
+//
+//============================================================================

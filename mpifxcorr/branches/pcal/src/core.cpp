154e223c01d0ce9dd30bca93278100ef42ace2b9
DiFX-DEV <FredericJaron@example.com>
2009-12-14 11:32:26 +0000

    Setting the offset to 0 to find out what it does.
    git-svn-id: https://svn.atnf.csiro.au/difx@1834 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- mpifxcorr/branches/pcal/src/core.cpp
+++ mpifxcorr/branches/pcal/src/core.cpp
@@ -660,6 +660,7 @@ void Core::processdata(int index, int threadid, int startblock, int numblocks, M
     
     if(config->getDPhaseCalIntervalMHz(procslots[index].configindex, j) > 0)
     {
+      /*
       // Calculate the sample time. Every band has the same bandwidth.
       sampletimens = 1.0/(2.0*config->getDRecordedBandwidth(procslots[index].configindex, j, 0))*1e+3;
       
@@ -671,6 +672,10 @@ void Core::processdata(int index, int threadid, int startblock, int numblocks, M
       
       // Calculate the number of offset samples. The modulo PCal bins is done in the pcal object!
       offsetsamples = starttimens/sampletimens + startblock*fftsize;
+      */
+      
+      // NOTE Setting the offset to 0 to see if it makes any difference...
+      offsetsamples = 0;
       
       modes[j]->resetpcal(offsetsamples);
     }

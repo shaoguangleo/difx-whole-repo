5ca57c298e712a47a745ef828cca4ae2aa4269bc
XhrisPhillips <Chris.Phillips@csiro.au>
2011-12-08 21:37:29 +0000

    Remove a whole heap of stuff
    git-svn-id: https://svn.atnf.csiro.au/difx@4078 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/difxio/branches/NRAO-DiFX-1.1/difxio.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIFXIO_H
-#define DIFXIO_H
-
-#include "difxio/parsedifx.h"
-#include "difxio/parsevis.h"
-#include "difxio/difx_input.h"
-
-#endif
--- master_tags/CIRA-DiFX-1.0/applications/calc/calc9/situvw.i
+++
@@ -1,7 +0,0 @@
-c
-c Include file to extract Site UVWs
-c
-      real*8	uvwp(3), uvwv(3)
-
-      common / situvw / uvwp, uvwv
-c

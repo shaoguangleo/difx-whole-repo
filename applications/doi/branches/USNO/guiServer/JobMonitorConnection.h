ba6d79623f66f5c3e81458666a858c8328b46b89
DiFX-DEV <john.spitzak.ctr@usno.navy.mil>
2012-05-30 17:25:53 +0000

    forgot this...
    git-svn-id: https://svn.atnf.csiro.au/difx@4596 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ applications/doi/branches/USNO/guiServer/JobMonitorConnection.h
@@ -0,0 +1,49 @@
+#ifndef GUISERVER_JOBMONITORCONNECTION_H
+#define GUISERVER_JOBMONITORCONNECTION_H
+//=============================================================================
+//
+//   guiServer::JobMonitorConnection Class
+//
+//!  Handles a (client) TCP connection with the GUI to send it job monitoring
+//!  diagnostics and to respond to some instructions.
+//
+//=============================================================================
+#include <PacketExchange.h>
+
+namespace guiServer {
+
+    class JobMonitorConnection : public PacketExchange {
+    
+    public:
+
+        //-----------------------------------------------------------------------------
+        //  These are packet types used for sending diagnostic and progress messages from
+        //  running jobs to the GUI.  These get rather highly specific.  There are a
+        //!  few types that are used to obtain data from the GUI as well.
+        //-----------------------------------------------------------------------------
+        static const int JOB_TERMINATED                     = 100;
+        static const int JOB_ENDED_GRACEFULLY               = 101;
+        static const int JOB_STARTED                        = 102;
+        static const int PARAMETER_CHECK_IN_PROGRESS        = 103;
+        static const int PARAMETER_CHECK_SUCCESS            = 104;
+        static const int FAILURE_NO_HEADNODE                = 105;
+        static const int FAILURE_NO_DATASOURCES             = 106;
+        static const int FAILURE_NO_PROCESSORS              = 107;
+        static const int FAILURE_NO_INPUTFILE_SPECIFIED     = 108;
+        static const int FAILURE_INPUTFILE_NOT_FOUND        = 109;
+        static const int FAILURE_INPUTFILE_NAME_TOO_LONG    = 110;
+    
+        JobMonitorConnection( network::GenericSocket* sock ) : PacketExchange( sock ) {
+        }
+        
+        ~JobMonitorConnection() {
+        }
+        
+
+    protected:
+            
+    };
+
+}
+
+#endif

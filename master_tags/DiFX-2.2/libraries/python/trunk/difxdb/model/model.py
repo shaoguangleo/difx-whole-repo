a55b5b1273272be2efc376876981aa260d06c50a
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2013-06-03 12:58:08 +0000

    Tagging for DiFX -2.2
    git-svn-id: https://svn.atnf.csiro.au/difx@5406 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ master_tags/DiFX-2.2/libraries/python/trunk/difxdb/model/model.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id$
+# $HeadURL$
+# $LastChangedRevision$
+# $Author$
+# $LastChangedDate$
+#
+#============================================================================
+class Module(object):
+    pass
+    
+class Slot (object):
+    pass
+     
+class Experiment(object):
+    pass
+
+class ExperimentStatus(object):
+    pass
+
+class Job(object):
+    pass
+
+class JobStatus(object):
+    pass
+
+class Pass(object):
+    pass
+
+class PassType(object):
+    pass
+
+class Queue(object):
+    pass
+
+class VersionHistory(object):
+    pass
+

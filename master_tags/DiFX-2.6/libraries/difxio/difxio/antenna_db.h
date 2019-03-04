10a127685866e5aaba69b8693bba643541cceeec
walterfb <wbrisken@gmail.com>
2019-03-04 15:51:33 +0000

    difxio to 2.6 branch
    git-svn-id: https://svn.atnf.csiro.au/difx@8706 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ master_tags/DiFX-2.6/libraries/difxio/difxio/antenna_db.h
@@ -0,0 +1,31 @@
+#ifndef __ANTENNA_DB_H__
+#define __ANTENNA_DB_H__
+
+#include <stdio.h>
+
+#define ANTENNA_DB_NAME_LENGTH		16
+#define ANTENNA_DB_MOUNT_LENGTH		8
+
+typedef struct
+{
+	char name[ANTENNA_DB_NAME_LENGTH];
+	double x, y, z;					/* ITRF coords [m]; note: not precise enough for correlation! */
+	double diameter;				/* [m] */
+	double arrayExtent;				/* [km] ; radius of array from the given xyz.  0.0 for single dishes */
+	char mountType[ANTENNA_DB_MOUNT_LENGTH];	/* leave blank for AzEl */
+} AntennaDBEntry;
+
+/* recognized mount types: "" -> "AZEL", "EQUA", "SPACE", "XYEW", "NASR", "NASL", "XYNS" */
+
+
+void fprintAntennaDBEntry(FILE *out, const AntennaDBEntry *ae);
+
+const AntennaDBEntry *antennaDBGetByIndex(unsigned int index);
+
+const AntennaDBEntry *antennaDBGetByXYZ(double x, double y, double z);
+
+const AntennaDBEntry *antennaDBGetByName(const char *name);
+
+void ecef2lla(double *lat, double *lon, double *alt, double x, double y, double z);
+
+#endif

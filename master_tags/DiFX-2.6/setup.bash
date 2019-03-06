6fffedd84ccfc65f1a079634f1bfca3491920355
XhrisPhillips <Chris.Phillips@csiro.au>
2019-03-06 05:51:40 +0000

    Tag as 2.6
    git-svn-id: https://svn.atnf.csiro.au/difx@8775 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.6/setup.bash
+++ master_tags/DiFX-2.6/setup.bash
@@ -1,5 +1,5 @@
 ####### DIFX VERSION ########################
-export DIFX_VERSION=trunk
+export DIFX_VERSION=DiFX-2.6
 
 ####### ROOT PATHS ##########################
 export DIFXROOT=/usr/local/difx

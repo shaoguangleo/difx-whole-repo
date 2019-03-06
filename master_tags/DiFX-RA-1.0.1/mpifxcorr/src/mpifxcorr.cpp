3ea6ed507bbe21c9a9a5a0bce67499f7b3957e30
jwagnerhki <jan.wagner@iki.fi>
2019-03-06 17:00:16 +0000

    r8781 patched into DiFX-RA-1.0.1 as well
    git-svn-id: https://svn.atnf.csiro.au/difx@8784 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-RA-1.0.1/mpifxcorr/src/mpifxcorr.cpp
+++ master_tags/DiFX-RA-1.0.1/mpifxcorr/src/mpifxcorr.cpp
@@ -243,8 +243,7 @@ int main(int argc, char *argv[])
 	string monitoropt;
 	pthread_t commandthread;
 	pthread_attr_t attr;
-	int nameslength = 1;
-	char monhostname[nameslength];
+	char monhostname[512];
 	int port=0, monitor_skip=0, namelen;
 	double restartseconds = 0.0;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -304,7 +303,7 @@ int main(int argc, char *argv[])
 				port = atoi(monitoropt.substr(colindex1 + 1, colindex2-colindex1-1).c_str());
 				monitor_skip = atoi(monitoropt.substr(colindex2 + 1).c_str());
 			}
-			strcpy(monhostname, monitoropt.substr(2,colindex1-2).c_str());
+			strncpy(monhostname, monitoropt.substr(2,colindex1-2).c_str(), sizeof(monhostname)-1);
 		}
 		else if(argv[i][0]=='-' && argv[i][1]=='r')
 		{

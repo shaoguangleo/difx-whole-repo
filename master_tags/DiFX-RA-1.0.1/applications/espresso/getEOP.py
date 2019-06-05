772c121f77b50fafd1e277e56752b357d1e7081e
jwagnerhki <jan.wagner@iki.fi>
2019-06-05 10:32:53 +0000

    dra101 update usno finals urls
    git-svn-id: https://svn.atnf.csiro.au/difx@8990 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-RA-1.0.1/applications/espresso/getEOP.py
+++ master_tags/DiFX-RA-1.0.1/applications/espresso/getEOP.py
@@ -1,81 +1,179 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
+# =======================================================================
+# Copyright (C) 2016 Cormac Reynolds
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# =======================================================================
+
 # Cormac Reynolds, December 2011: program to fetch 5 days of EOPs around the
-# given date and return in format suitable for appending to .v2d file.
-# Takes dates in MJD or VEX format.
-# EOP data from http://gemini.gsfc.nasa.gov/solve_save/usno_finals.erp
-# Leap second data come from: http://gemini.gsfc.nasa.gov/500/oper/solve_apriori_files/ut1ls.dat
+# given date and return in format suitable for appending to .v2d or .vex file.
+# Takes dates in MJD, VEX, ISO8601 or VLBA format.
+# October 2017: change to use requests module. Optional vex output.
+# May 2019: New download site.
+#
+# EOP/ut1 data from:
+# "ftp://cddis.gsfc.nasa.gov/vlbi/gsfc/ancillary/solve_apriori"
+#
 
-import optparse, re, urllib, espressolib, sys, time
+from __future__ import print_function, division
+import sys
+import time
+import os
+import optparse
+import urllib2
+#import requests
+import espressolib
 
-def get_leapsec(leapsec_page, targetJD):
-    # parse the leap seconds page
+
+def get_leapsec(leapsec_page, target_jd):
+    """parse the leap seconds page"""
+
+    tai_utc = None
     for line in leapsec_page:
         linedate = float(line[17:27])
-        if linedate > targetJD:
+        if linedate > target_jd:
             break
         else:
-            tai_utc = float(line[38:49])
+            tai_utc = int(float(line[38:49]))
     return tai_utc
 
-usage = '''%prog <date>
-<date> can either be MJD or VEX time
-Returns 5 days of EOPs around <date> in .v2d format'''
-parser = optparse.OptionParser(usage=usage, version='%prog ' + '1.0')
+
+usage = """%prog <date>
+<date> can either be in MJD, VEX, ISO8601 or VLBA format
+Returns 5 days of EOPs around <date> in .v2d format"""
+parser = optparse.OptionParser(usage=usage, version="%prog " + "1.0")
+parser.add_option(
+        "--vex", "-v",
+        dest="vex", action="store_true", default=False,
+        help="Produce vex format output")
+parser.add_option(
+        "--local", "-l",
+        dest="local", action="store_true", default=False,
+        help="Take EOPs from local $DIFX_EOPS and $DIFX_UT1LS files instead of"
+        " web")
+parser.add_option(
+        "--noverify",
+        dest="verify", action="store_false", default=True,
+        help="Disable HTTPS certificate checking (at own risk!)")
+
 (options, args) = parser.parse_args()
 
 if len(args) != 1:
     parser.print_help()
     parser.error("Give a date in MJD or VEX format")
 
+if options.vex:
+    comment = "*"
+else:
+    comment = "#"
 
+mjd_jd = 2400000.5
 # get target MJD
-targetMJD = args[0];
+target_mjd = args[0]
 # convert to MJD if necessary
-targetMJD = espressolib.convertdate(targetMJD, 'mjd')
+target_mjd = espressolib.convertdate(target_mjd, "mjd")
+target_jd = round(target_mjd) + mjd_jd
 
-#print targetMJD
-
-targetJD  = round(targetMJD) + 2400000.5;
 
 # dates before June 1979 not valid (earliest EOPs)
-if (targetJD < 2444055.5):
-    raise Exception('Date too early. No valid EOPs before July 1979')
+if (target_jd < 2444055.5):
+    raise Exception("Date too early. No valid EOPs before July 1979")
 
 # fetch EOP data
-eop_url = "http://gemini.gsfc.nasa.gov/solve_save/usno_finals.erp"
-leapsec_url = "http://gemini.gsfc.nasa.gov/500/oper/solve_apriori_files/ut1ls.dat"
+leapsec_page = None
+if not options.local:
+    gsfc_url = "ftp://cddis.gsfc.nasa.gov/vlbi/gsfc/ancillary/solve_apriori/"
+    eop_url = gsfc_url + "usno_finals.erp"
+    leapsec_url = gsfc_url + "ut1ls.dat"
 
-print >>sys.stderr, "Fetching EOP data..."
-eop_page = urllib.FancyURLopener().open(eop_url).readlines()
-print >>sys.stderr, "...got it."
+    sys.stderr.write("Fetching EOP data from {:s}\n".format(eop_url))
+    #eop_page = requests.get(eop_url, verify=options.verify).content.split("\n")
+    eop_page = urllib2.urlopen(eop_url).readlines()
 
-print >>sys.stderr, "Fetching Leap second data..."
-leapsec_page = urllib.FancyURLopener().open(leapsec_url).readlines()
-print >>sys.stderr, "...got it.\n";
+    sys.stderr.write(
+            "Fetching leap second data from {:s}\n".format(leapsec_url))
+    #leapsec_page = requests.get(
+    #        leapsec_url, verify=options.verify).content.split("\n")
+    leapsec_page = urllib2.urlopen(leapsec_url).readlines()
+    print ("{:s} EOPs downloaded at {:s}".format(
+            comment, time.strftime("%Y-%m-%d %H:%M:%S (%z)")))
+else:
+    # or read from local files
+    eop_filename = os.environ.get("DIFX_EOPS")
+    assert eop_filename is not None, "You must set $DIFX_EOPS"
+    sys.stderr.write("Reading EOP data from {:s}\n".format(eop_filename))
+    eop_page = open(eop_filename).readlines()
+    eop_page_stats = os.stat(eop_filename)
+    eop_update_time = time.gmtime(os.stat(eop_filename)[9])
+    leapsec_filename = os.environ.get("DIFX_UT1LS")
+    assert leapsec_filename is not None, "You must set $DIFX_UT1LS"
+    sys.stderr.write(
+            "Reading leap second data from {:s}\n".format(leapsec_filename))
+    leapsec_page = open(leapsec_filename).readlines()
+    print (
+            "{:s} EOPS from {:s} last updated at {:s}".format(
+            comment, eop_filename,
+            time.strftime("%Y-%m-%d %H:%M:%S (%z)", eop_update_time)))
 
+v2deop = "EOP {0:d} {{ xPole={1:f} yPole={2:f} tai_utc={3:d} ut1_utc={4:f} }}"
 
-#print "#EOPs downloaded at", datetime.datetime.now()
-print "#EOPs downloaded at", time.strftime('%Y-%m-%d %H:%M:%S (%z)')
+vexeop = """def EOP{5:d};
+  TAI-UTC = {3:d};
+  A1-TAI = 0 sec;
+  eop_ref_epoch = {0:s};
+  num_eop_points = 1;
+  eop_interval = 24 hr;
+  ut1-utc = {4:0.6f};
+  x_wobble = {1:0.6f} asec;
+  y_wobble = {2:0.6f} asec;
+enddef;"""
+
+if options.vex:
+    print ("$EOP;")
+    eopformat = vexeop
+else:
+    eopformat = v2deop
 
 # parse the eop page
-nlines = 0
-for line in eop_page:
-    nlines += 1
-    # skip the first line, which isn't commented for some reason
-    if (nlines == 1):
+neop = -1
+nlines = None
+for nlines, line in enumerate(eop_page):
+    # skip the first line, which isn't commented
+    if (nlines == 0):
+        continue
+    if not line:
         continue
     # strip comments
-    if (line[0] == '#'):
+    if (line[0] == "#"):
         continue
     # split the line on whitespace and convert to floats
     eop_fields = line.split()
     eop_fields = [float(field) for field in eop_fields]
     # print an EOP line if we're within 3 days of the target day
-    if (abs(eop_fields[0] - targetJD) < 3):
-        tai_utc = None;
+    if (abs(eop_fields[0] - target_jd) < 3):
+        neop += 1
         tai_utc = get_leapsec(leapsec_page, eop_fields[0])
-        if not tai_utc:
-            raise Exception("Leap seconds not found! Check your dates")
-        print "EOP %d { xPole=%f yPole=%f tai_utc=%d ut1_utc=%f }" % (eop_fields[0]-2400000.5,eop_fields[1]/10.,eop_fields[2]/10.,tai_utc,tai_utc+eop_fields[3]/1000000.)
+        if tai_utc is None:
+            raise Exception("Leap seconds not found! Check your UT1LS file")
+        xpole = eop_fields[1]/10.
+        ypole = eop_fields[2]/10.
+        ut1_utc = tai_utc+eop_fields[3]/1.e6
+        eopdate = int(eop_fields[0] - mjd_jd)
+        if options.vex:
+            eopdate = espressolib.convertdate(eopdate, outformat="vex")
+        print (eopformat.format(eopdate, xpole, ypole, tai_utc, ut1_utc, neop))
 
-print >>sys.stderr, "Processed %d lines" % nlines;
+sys.stderr.write("Processed {0:d} lines\n".format(nlines))

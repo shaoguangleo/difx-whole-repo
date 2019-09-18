ae396e943271d87b97bb5133b5098f8dfc20d449
jwagnerhki <jan.wagner@iki.fi>
2019-09-18 09:07:18 +0000

    merge d2f --union fix r9145 into d261
    git-svn-id: https://svn.atnf.csiro.au/difx@9167 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.6.1/libraries/difxio/difxio/difx_input_merge.c
+++ master_tags/DiFX-2.6.1/libraries/difxio/difxio/difx_input_merge.c
@@ -152,6 +152,30 @@ int areDifxInputsCompatible(const DifxInput *D1, const DifxInput *D2, const Difx
 				return 0;
 			}
 		}
+
+		if (D1->nIF != 0 && D2->nIF != 0 && D1->nIF != D2->nIF)
+		{
+			++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
+			return 0;
+		}
+
+		for(f = 0; f < D1->nFreqSet; ++f)
+		{
+			if (D1->freqSet[f].nIF != D1->nIF)
+			{
+				++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
+				return 0;
+			}
+		}
+		for(f = 0; f < D2->nFreqSet; ++f)
+		{
+			if (D2->freqSet[f].nIF != D2->nIF)
+			{
+				++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
+				return 0;
+			}
+		}
+
 		break;
 	case FreqMergeModeUnion:
 		/* nothing to check here */
@@ -178,7 +202,7 @@ int areDifxInputsCompatible(const DifxInput *D1, const DifxInput *D2, const Difx
 		{
 			if(isSameDifxAntenna(D1->antenna + a1, D2->antenna + a2))
 			{
-				switch(mergeOptions->clockMergeMode)
+			        switch(mergeOptions->clockMergeMode)
 				{
 				case ClockMergeModeStrict:
 					if(!isSameDifxAntennaClock(D1->antenna + a1, D2->antenna + a2))
@@ -199,29 +223,6 @@ int areDifxInputsCompatible(const DifxInput *D1, const DifxInput *D2, const Difx
 					exit(0);
 				}
 			}
-		}
-	}
-
-	if (D1->nIF != 0 && D2->nIF != 0 && D1->nIF != D2->nIF)
-	{
-		++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
-		return 0;
-	}
-
-	for(f = 0; f < D1->nFreqSet; ++f)
-	{
-		if (D1->freqSet[f].nIF != D1->nIF)
-		{
-			++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
-			return 0;
-		}
-	}
-	for(f = 0; f < D2->nFreqSet; ++f)
-	{
-		if (D2->freqSet[f].nIF != D2->nIF)
-		{
-			++difxInputCompatibilityStatistics[DifxInputCompatibilityFreqSet];
-			return 0;
 		}
 	}
 

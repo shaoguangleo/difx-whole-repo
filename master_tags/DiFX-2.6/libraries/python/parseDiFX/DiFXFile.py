5b90ce3e19f6e42ada4dbeea7d12d3787e4749b9
jwagnerhki <jan.wagner@iki.fi>
2020-04-15 14:58:41 +0000

    parseDiFX r9400 r9473 into d260
    git-svn-id: https://svn.atnf.csiro.au/difx@9484 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.6/libraries/python/parseDiFX/DiFXFile.py
+++ master_tags/DiFX-2.6/libraries/python/parseDiFX/DiFXFile.py
@@ -36,6 +36,7 @@ class DiFXFile:
         self.visrecord = VisibilityRecord()
         self.inputfilename = ''
         self.difxfilename = ''
+        self.difxfilepath = ''
         self.difxfile = None
         self.valid = False
         if inputfilename:
@@ -47,16 +48,15 @@ class DiFXFile:
         if not self.valid:
             return
         
-        glob_pattern = self.metainfo.common['difxfile'] + '/DIFX_*.s*.b*'
+        self.difxfilepath = self.metainfo.common['difxfile']
+
+        glob_pattern = self.difxfilepath + '/DIFX_*.s*.b*'
         difxfileslist = glob.glob(glob_pattern)
-
         if len(difxfileslist) <= 0:
-            print ('Error: no visibility data file found in %s!' % (glob_pattern))
-            raise ValueError('No visibility files %s as referenced by %s were found' % (glob_pattern,inputfilename))
-
-        self.difxfilename = difxfileslist[0]
-        self.difxfile = open(self.difxfilename, 'rb')
-        self.valid = True
+            print ('Warning: no visibility data file found in %s!' % (glob_pattern))
+        else:
+            self.difxfilename = difxfileslist[0]
+            self.difxfile = open(self.difxfilename, 'rb')
 
     def close(self):
         self.difxfile.close()
@@ -68,7 +68,8 @@ class DiFXFile:
         return self.visrecord
 
     def nextVisibilityRecord(self):
-        self.visrecord.fromfile(self.difxfile, self.metainfo.freqs)
+        if self.difxfile is not None:
+            self.visrecord.fromfile(self.difxfile, self.metainfo.freqs)
         return self.visrecord
 
     def getFrequency(self, freqindex):

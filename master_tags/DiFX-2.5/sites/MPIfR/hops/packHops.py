5db7ca13f78453247f3ac0f5f323d83143a4c9d4
jwagnerhki <jan.wagner@iki.fi>
2021-03-08 17:11:19 +0000

    d250 MPIfR add copy of distFourfit.py, fplot2pdf, packHops.py, plotResiduals.py
    git-svn-id: https://svn.atnf.csiro.au/difx@9971 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ master_tags/DiFX-2.5/sites/MPIfR/hops/packHops.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: latin-1
+#===========================================================================
+# Copyright (C) 2017  Max-Planck-Institut für Radioastronomie, Bonn, Germany
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id: packHops.py 8200 2018-03-20 12:11:46Z HelgeRottmann $
+# $HeadURL: https://svn.atnf.csiro.au/difx/sites/MPIfR/hops/packHops.py $
+# $LastChangedRevision: 8200 $
+# $Author: HelgeRottmann $
+# $LastChangedDate: 2018-03-20 13:11:46 +0100 (Tue, 20 Mar 2018) $
+#
+#============================================================================
+
+import sys
+import os
+import re
+import tarfile
+import argparse
+from shutil import rmtree
+
+reHops = re.compile("^\d{4}$")
+
+tarDirs = []
+
+description = "Program to search for fourfit subdirectories (e.g. 1234) recursively. All the discovered directories will be tar'ed, gzip'ed and the original directory will be removed."
+
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('-z', "--zip", action='store_true' , dest="compress", default=False, help='Compress tar-archive with gzip.')
+parser.add_argument('-l', "--list", action='store_true' , dest="list", default=False, help='Only list discovered HOPS subdirectories. Do not pack.')
+parser.add_argument('rootdir', help='The root directory underneath which to start finding the HOPS directories.')
+args = parser.parse_args()
+
+print "Searching for HOPS subdirectories...."
+for subdir, dirs, files in os.walk(args.rootdir):
+    for dir in dirs:
+        # check for fourfit subdirectories (4-digit numeric)
+        if reHops.match(dir):
+          print "Found: " + subdir + "/" + dir
+          tarDirs.append(subdir + "/" + dir)
+
+if args.list:
+	sys.exit(0)
+	
+
+for dir in tarDirs:
+
+    # check that path exist (could have been packed already)
+    if os.path.isdir(dir) == False:
+	continue
+
+    path,code = os.path.split(dir)
+
+    if args.compress:
+      filename = dir + ".tar.gz"
+      mode = "w:gz"
+    else:
+      filename = dir + ".tar"
+      mode = "w"
+
+    print "creating: " + filename
+    # open tar file (will overwrite if exist)
+    try:
+      tar = tarfile.open (filename, mode)
+      tar.add(dir, arcname=code)
+      tar.close()
+    except:
+      sys.exit("An error has occured when creating the tarball: " + filename)
+  
+    # remove hops subdirectory
+    try:
+        print "Removing: " + dir
+        rmtree(dir)
+    except OSError as ex:
+        print(ex)
+
+print "Done"

18fcd55a31277e0c606a29e756fbba50f4501f10
XhrisPhillips <Chris.Phillips@csiro.au>
2021-09-30 12:49:24 +0000

    Bump version
    git-svn-id: https://svn.atnf.csiro.au/difx@10215 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.6.2/setup.bash
+++ master_tags/DiFX-2.6.2/setup.bash
@@ -1,5 +1,5 @@
 ####### DIFX VERSION ########################
-export DIFX_VERSION=DiFX-2.6.2
+export DIFX_VERSION=DiFX-2.6.3
 
 ####### ROOT PATHS ##########################
 export DIFXROOT=/usr/local/difx

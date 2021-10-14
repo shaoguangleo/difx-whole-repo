9cc288ad9533fa28146f5a0eb798ef2a90744a6c
gbcrew <gbc@haystack.mit.edu>
2021-10-14 22:27:34 +0000

    Delivering HOPS 3.23 vendor branch to 2.6.3 from r10257
    git-svn-id: https://svn.atnf.csiro.au/difx@10258 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.6.3/applications/hops/data/ff_testdata/chk_ff_2843.sh
+++ master_tags/DiFX-2.6.3/applications/hops/data/ff_testdata/chk_ff_2843.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# $Id: chk_ff_2843.sh 1032 2015-01-23 14:24:03Z gbc $
+# $Id: chk_ff_2843.sh 3327 2021-09-04 13:47:06Z gbc $
 #
 # canonical test suite for fourfit
 #
@@ -15,7 +15,8 @@ export DATADIR=`cd $srcdir/testdata; pwd`
 rm -f ff-2843.ps
 $verb && echo \
 fourfit -t -d diskfile:ff-2843.ps -b AI:S \\ && echo \
-    $DATADIR/2843/321-1701_0552+398/0552+398.oifhak
+    $DATADIR/2843/321-1701_0552+398/0552+398.oifhak \\ && echo \
+    set start -3
 
 # AIT
 fourfit -t -d diskfile:ff-2843.ps -b AI:S \
@@ -23,19 +24,27 @@ fourfit -t -d diskfile:ff-2843.ps -b AI:S \
     set start -3 2>/dev/null 1>&2
 [ -f ./ff-2843.ps ] || { echo ./ff-2843.ps missing && exit 2 ; }
 
-# pluck out line containing the snr and parse it
-line=$(grep '7570 9653' ./ff-2843.ps)
+# pluck out lines containing the snr and amp, parse and check
 
+# ff-2843.ps:7570 9384 M (21.664) SR
+line=$(grep '7570 9384' ./ff-2843.ps)
 IFS='()'
-read a snr b <<<"$line"
+read a amp b <<<"$line"
+low=21.660
+high=21.668
+okamp=$(echo "$amp>$low && $amp<$high" | bc)
+$verb && echo okamp is $okamp and "$low < $amp < $high" is expected from: $line
+# ff-2843.ps:7570 9653 M (48.2) SR
+lsnr=$(grep '7570 9653' ./ff-2843.ps)
+IFS='()'
+read a snr b <<<"$lsnr"
+low=48.1
+high=48.3
+oksnr=$(echo "$snr>$low && $snr<$high" | bc)
+$verb && echo oksnr is $oksnr and "$low < $snr < $high" is expected from: $lsnr
+#
+[ "$okamp" -gt 0 -a "$oksnr" -gt 0 ]
 
-# snr bounds
-low=47.8
-high=48.6
-aok=$(echo "$snr>$low && $snr<$high" | bc)
-$verb && echo aok is $aok and "$low < $snr < $high" is expected from: $line
-
-[ "$aok" -gt 0 ]
 #
 # eof
 #
--- master_tags/DiFX-2.6.3/applications/hops/sub/util/check_name.c
+++ master_tags/DiFX-2.6.3/applications/hops/sub/util/check_name.c
@@ -76,6 +76,7 @@ check_name(char *name, fstruct *f_info)
         }
 
     if (nfield == 1) return (BADSTRING);
+    (void) field5; //disable unused var compiler warning
 
     switch (nfield)
         {
@@ -153,8 +154,8 @@ check_name(char *name, fstruct *f_info)
                                         /* Filenumbers only for type 2's in mk4 */
     if ((filenum != NULL) && (f_info->type == 2))
         {
-        l = strlen(filenum);                            /* File #, e.g. (1-999) */
-        if ((l == 0) || (l > 3)) errcode |= BADFNUM;
+        l = strlen(filenum);                            /* File #, e.g. (1-9999) */
+        if ((l == 0) || (l > 4)) errcode |= BADFNUM;
         l--;
         for ( ; l >= 0; l--)
             if (! isdigit (filenum[l])) errcode |= BADFNUM;

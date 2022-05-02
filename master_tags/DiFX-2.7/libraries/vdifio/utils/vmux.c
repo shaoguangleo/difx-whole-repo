14df1556dfd28bb807a2397df8cf6e6f91699156
jwagnerhki <jan.wagner@iki.fi>
2022-05-02 19:13:02 +0000

    d270 vdifio get r10449 vmux change and other updates from difx-2.6.3
    git-svn-id: https://svn.atnf.csiro.au/difx@10454 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.7/libraries/vdifio/utils/vmux.c
+++ master_tags/DiFX-2.7/libraries/vdifio/utils/vmux.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013-2019 by Walter Brisken                             *
+ *   Copyright (C) 2013-2022 by Walter Brisken                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -35,8 +35,8 @@
 
 const char program[] = "vmux";
 const char author[]  = "Walter Brisken <wbrisken@nrao.edu>";
-const char version[] = "0.10";
-const char verdate[] = "20191031";
+const char version[] = "0.10.1";
+const char verdate[] = "20220429";
 
 const int defaultChunkSize = 10000000;
 const int defaultNGap = 100;
@@ -89,9 +89,9 @@ void usage(const char *pgm)
 	fprintf(stderr, "  --quiet\n");
 	fprintf(stderr, "  -q        Decrease verbosity\n\n");
 	fprintf(stderr, "  --noEDV4\n");
-	fprintf(stderr, "  -n        Don't make use of EDV4 (per-thread validity) in output\n\n");
+	fprintf(stderr, "  -n        Don't make use of EDV4 (per-thread validity) in output [default]\n\n");
 	fprintf(stderr, "  --EDV4\n");
-	fprintf(stderr, "  -e        Use EDV4 (per-thread validity) in output [default]\n\n");
+	fprintf(stderr, "  -e        Use EDV4 (per-thread validity) in output (use with caution)\n\n");
 	fprintf(stderr, "  --fanout <f>\n");
 	fprintf(stderr, "  -f <f>    Set fanout factor to <f> (used for some DBBC3 data) [default = 1]\n\n");
 	fprintf(stderr, "  --gap <g>\n");
@@ -159,7 +159,7 @@ int main(int argc, char **argv)
 	int fanoutFactor = 1;
 	const vdif_header *vh;
 	struct vdif_mux vm;
-	int flags = VDIF_MUX_FLAG_PROPAGATEVALIDITY | VDIF_MUX_FLAG_RESPECTGRANULARITY;
+	int flags = VDIF_MUX_FLAG_RESPECTGRANULARITY;
 	int a;
 
 	if(argc <= 1)
@@ -582,7 +582,7 @@ int main(int argc, char **argv)
 		}
 
 		/* if we encountered fill pattern at the seam between two chunkSizes we will need to write some dummy frames */
-		if(nextFrame >= 0 && nextFrame != stats.startFrameNumber)
+		if(stats.destUsed > 0 && nextFrame >= 0 && nextFrame != stats.startFrameNumber)
 		{
 			int nJump = (int)(stats.startFrameNumber - nextFrame);
 			int j;
@@ -593,7 +593,7 @@ int main(int argc, char **argv)
 			memcpy(src, dest, VDIF_HEADER_BYTES);
 			for(j = 0; j < nJump; ++j)
 			{
-				setVDIFFrameSecond((vdif_header *)src, (nextFrame+j)/framesPerSecond);
+				((vdif_header *)src)->seconds = (nextFrame+j)/framesPerSecond;
 				setVDIFFrameNumber((vdif_header *)src, (nextFrame+j)%framesPerSecond);
 				setVDIFFrameInvalid((vdif_header *)src, 1);
 				fwrite(src, 1, stats.outputFrameSize, out);

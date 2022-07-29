24f715b7b4ac938cd86f876768b2bdaee08e7695
XhrisPhillips <Chris.Phillips@csiro.au>
2022-07-29 02:27:58 +0000

    Actually do packet stats
    git-svn-id: https://svn.atnf.csiro.au/difx@10563 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/codifio/branches/version2/utils/codif_write.c
+++ libraries/codifio/branches/version2/utils/codif_write.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *  Copyright (C) 2018-2019 by Chris Phillips                              *
+ *  Copyright (C) 2018-2022 by Chris Phillips                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -31,12 +31,7 @@
 
   codif_write
 
-  Read UDP packets from an ADE PAF with "Streaming" Firmware and write resulting data to local file
-  Data is assumed to be 16bit complex and a lot of fixes are made to the data based on the PAF
-  format. 
-
-  Do not use on "generic" codif data stream  - this is unlikely to work
-
+  Read UDP packets from a CODIF stream and write resulting data to local file
 
  **********************************************************************************************/
 
@@ -101,27 +96,42 @@
 #define DEFAULT_FILESIZE    60
 #define DEFAULT_UPDATETIME  1.0
 #define UPDATE              20
-#define MAXTHREAD           20
+#define MAXTHREAD           30
 
 #define DEBUG(x) 
 
 double tim(void);
 void alarm_signal (int sig);
 
-int setup_net(unsigned short port, const char *ip, int *sock);
-int openfile(char *fileprefix, char *timestr, int threadid);
 
 volatile int time_to_quit = 0;
 volatile int sig_received = 0;
 
-#define INIT_STATS() { \
-  npacket = 0; \
-  maxpkt_size = 0; \
-  minpkt_size = ULONG_MAX; \
-  sum_pkts = 0; \
-  pkt_drop = 0; \
-  pkt_oo = 0; \
-  skipped = 0; \
+typedef struct datastats {
+  int threadid;
+  uint32_t lastsecond;
+  uint32_t lastframe;
+  uint32_t maxframe;
+  uint64_t npacket;
+  uint64_t sum_pkts;
+  uint64_t pkt_drop;
+  uint64_t pkt_oo;
+  int maxpkt_size;
+  int minpkt_size;
+} datastats;
+  
+int setup_net(unsigned short port, const char *ip, int *sock);
+int matchthread(datastats *allstats, int nthread, int threadID);
+int openfile(char *fileprefix, char *timestr, int threadid);
+
+void initstats (datastats *stats) { 
+  stats->npacket = 0;	   
+  stats->maxpkt_size = 0; 
+  stats->minpkt_size = INT_MAX; 
+  stats->maxframe = 0;
+  stats->sum_pkts = 0; 
+  stats->pkt_drop = 0;
+  stats->pkt_oo = 0; 
 }
 
 /* Globals needed by alarm handler */
@@ -130,12 +140,15 @@ unsigned long minpkt_size, maxpkt_size, pkt_drop, pkt_oo;
 uint64_t sum_pkts, pkt_head, npacket, skipped;
 double t0, t1, t2;
 
+int nthread = 0;
+datastats allstats[MAXTHREAD];
+
 int lines = 0;
 
 int main (int argc, char * const argv[]) {
   char *buf, timestr[MAXSTR], filename[MAXSTR];
-  int tmp, opt, status, sock, ofile[MAXTHREAD], skip, i, nthread=0, threads[MAXTHREAD];
-  int ithread, thisthread = -1;
+  int threadIndex, tmp, opt, status, sock, ofile[MAXTHREAD], skip, i;
+  int groupID, valid, period, thisthread = -1;
   ssize_t nread, nwrote, nwrite;
   char msg[MAXSTR];
   float updatetime, ftmp;
@@ -145,7 +158,8 @@ int main (int argc, char * const argv[]) {
   codif_header *cheader=NULL;
   int16_t *s16, *cdata;
   int8_t *s8;
-  uint64_t *h64;
+  uint32_t lastseconds, lastframe, thisframe, thisseconds, framesperperiod;
+  //uint64_t *h64;
 
   unsigned int port = DEFAULT_PORT;
   char *ip = NULL;
@@ -155,8 +169,9 @@ int main (int argc, char * const argv[]) {
   int padding = 0;
   int threadid = -1;
   int scale = 0;
-  int invert = 0;
+  //int invert = 0;
   int splitthread = 0;
+  int verbose = 0;
   
   struct option options[] = {
     {"port", 1, 0, 'p'},
@@ -168,13 +183,14 @@ int main (int argc, char * const argv[]) {
     {"updatetime", 1, 0, 'u'},
     {"filesize", 1, 0, 'f'},
     {"scale", 1, 0, 's'},
-    {"invert", 0, 0, 'I'},
+    //{"invert", 0, 0, 'I'},
     {"split", 0, 0, 'S'},
+    {"verbose", 0, 0, 'V'},
     {"help", 0, 0, 'h'},
     {0, 0, 0, 0}
   };
 
-  for (i=0;i<MAXTHREAD;i++) ofile[i] = 0;
+  for (i=0;i<MAXTHREAD;i++) ofile[i] = -1;
   
   updatetime = DEFAULT_UPDATETIME;
 
@@ -248,14 +264,18 @@ int main (int argc, char * const argv[]) {
       fileprefix = strdup(optarg);
       break;
 
-    case 'I':
-      invert = 1;
-      break;
+      //    case 'I':
+      //invert = 1;
+      //break;
 
     case 'S':
       splitthread = 1;
       break;
 
+    case 'V':
+      verbose = 1;
+      break;
+      
     case 'h':
       printf("Usage: udp_write [options]\n");
       printf("  -p/-port <PORT>        Port to use\n");
@@ -266,7 +286,8 @@ int main (int argc, char * const argv[]) {
       printf("  -t/-time <N>           Record for N seconds\n");
       printf("  -f/-filesize <N>       Write files N seconds long\n");
       printf("  -s/-scale <S>          Reduce data to 8 bits, dividing by S\n");
-      printf("  -S/-split              Write threads to separate files\n"); 
+      printf("  -S/-split              Write threads to separate files\n");
+      printf("  -V/-verbose            Verbose output/n");
       printf("  -h/-help               This list\n");
       return(1);
     break;
@@ -294,14 +315,14 @@ int main (int argc, char * const argv[]) {
   }
 
   cheader = (codif_header*)buf;
-  h64 = (uint64_t*)cheader; // Header as 64bit worlds
+  //h64 = (uint64_t*)cheader; // Header as 64bit worlds
   cdata = (int16_t*) &buf[CODIF_HEADER_BYTES];
 
   status = setup_net(port, ip, &sock);
     
   if (status)  exit(1);
 
-  INIT_STATS();
+  initstats(&allstats[0]);
   pkt_head = 0;
 
   /* Install an alarm catcher */
@@ -318,7 +339,6 @@ int main (int argc, char * const argv[]) {
 
   int first = 1;
   while (t2-t0<time) {
-
     nread = recvfrom(sock, buf, MAXPACKETSIZE, MSG_WAITALL, 0, 0);
     if (nread==-1) {
       perror("Receiving packet");
@@ -330,10 +350,9 @@ int main (int argc, char * const argv[]) {
       fprintf(stderr, "Error: Packetsize larger than expected!\n");
       exit(1);
     } else if ((nread-padding) % 4) {
-      fprintf(stderr, "Error: Needs to read multiple of 32 bytes\n");
+      fprintf(stderr, "Error: Needs to read multiple of 4 bytes\n");
       exit(1);
     }
-
     // Block alarm signal while we are updating these values
     status = sigprocmask(SIG_BLOCK, &set, NULL);
     if (status) {
@@ -341,21 +360,81 @@ int main (int argc, char * const argv[]) {
       exit(1);
     }
 
-    thisthread = cheader->threadid;
-
-    npacket++;
-    if (nread>maxpkt_size) maxpkt_size = nread;
-    if (nread<minpkt_size) minpkt_size = nread;
-    sum_pkts += nread;
+    thisframe = getCODIFFrameNumber(cheader);
+    thisseconds = getCODIFFrameSecond(cheader);
+    thisthread = getCODIFThreadID(cheader);
 
     skip = 0;
     if (threadid>=0) {
-      if (cheader->threadid!=threadid) {
+      if (thisthread!=threadid) {
 	skip=1;
 	skipped++;
       }
     }
 
+    if (!skip) {
+      valid = 1;
+      if (first) {
+	first = 0;
+	threadIndex = 0;
+	allstats[0].threadid = thisthread;
+	nthread = 1;
+	
+	period = getCODIFPeriod(cheader);
+	int completesamplebits = getCODIFNumChannels(cheader)*getCODIFBitsPerSample(cheader);
+	if (getCODIFComplex(cheader)) completesamplebits *= 2;
+	
+	uint64_t totalsamples = getCODIFTotalSamples(cheader);
+	uint64_t totalbits = totalsamples * completesamplebits;  // Risk of overflow here
+
+	int framebytes = getCODIFFrameBytes(cheader); 
+	
+	framesperperiod = totalbits/(framebytes*8);
+	
+      } else {
+	threadIndex = matchthread(allstats, nthread, thisthread);
+	if (threadIndex==-1) {
+	  if (nthread==MAXTHREAD) {
+	    fprintf(stderr, "Error: Too many Threads. Increase MAXTHREAD (%d)\n", MAXTHREAD);
+	    exit(1);
+	  }
+	  nthread++;
+	  threadIndex = nthread -1;
+	  allstats[threadIndex].threadid = thisthread;
+	  initstats(&allstats[threadIndex]);
+	} else {
+	  lastseconds = allstats[threadIndex].lastsecond;
+	  lastframe = allstats[threadIndex].lastframe;
+	  if (thisseconds==lastseconds && thisframe==(lastframe+1)) {
+	    allstats[threadIndex].lastframe = thisframe;
+	  } else if (thisframe==0 && lastframe == (framesperperiod-1) && (thisseconds == (lastseconds+period))) {
+	  } else {
+	    if (thisseconds<lastseconds || (thisseconds==lastseconds && thisframe < lastframe)) {
+	      valid = 0;
+	      allstats[threadIndex].pkt_oo++;
+	      allstats[threadIndex].pkt_drop--;
+	    } else {
+	      groupID = getCODIFGroupID(cheader);
+
+
+	      if (verbose) printf("Thread %d (%d) dropped packet (%d/%d - %d/%d) %d\n", thisthread, groupID, thisframe, thisseconds, lastframe, lastseconds, (thisseconds-lastseconds)*framesperperiod + (thisframe-lastframe)-1);
+	      allstats[threadIndex].pkt_drop += (thisseconds-lastseconds)*framesperperiod + (thisframe-lastframe-1);
+	    }
+	  }
+	}
+      }
+      if (valid) {
+	allstats[threadIndex].lastsecond = thisseconds;
+	allstats[threadIndex].lastframe = thisframe;
+	allstats[threadIndex].npacket++;
+	if (nread>allstats[threadIndex].maxpkt_size) allstats[threadIndex].maxpkt_size = nread;
+	if (nread<allstats[threadIndex].minpkt_size) allstats[threadIndex].minpkt_size = nread;
+	allstats[threadIndex].sum_pkts += nread;
+      }
+
+      npacket++;
+      sum_pkts += nread;
+    }
     // Unblock the signal again
     status = sigprocmask(SIG_UNBLOCK, &set, NULL);
     if (status) {
@@ -365,80 +444,7 @@ int main (int argc, char * const argv[]) {
 
     if (skip) continue;
 
-    // Convert header then data to little endian - Note this is disabled above with #def
-    for (i=0; i<8; i++) {
-      h64[i] = byteswap_64(h64[i]);
-    }
-    if (invert) {
-      for (i=0; i<(nread-CODIF_HEADER_BYTES)/sizeof(int16_t);i++) {
-	cdata[i] = byteswap_16(cdata[i]);
-	i++;
-	cdata[i] = -byteswap_16(cdata[i]);
-      }
-    } else {
-      for (i=0; i<(nread-CODIF_HEADER_BYTES)/sizeof(int16_t);i++) {
-	cdata[i] = byteswap_16(cdata[i]);
-      }
-    }
-    t2 = tim();
-
-    if (first || t2-filetime>filesize) {
-      if (first) {
-	filetime = t0;
-	first = 0;
-      } else {
-	while (t2-filetime>filesize) {
-	  filetime += filesize;
-	}
-	if (splitthread) {
-	  for (i=0;i<nthread;i++) {
-	    close(ofile[i]);
-	    ofile[i] = 0;
-	    nthread = 0;
-          }
-	} else {
-	  close(ofile[0]);
-	  ofile[0] = 0;
-	}
-      }
-      time_t itime = (time_t)floor(filetime);
-      struct tm *date = gmtime(&itime); 
-
-      strftime(timestr, MAXSTR-1, "%j_%H%M%S", date);
-
-      if (splitthread) {
-	ofile[0] = openfile(fileprefix, timestr, thisthread);
-	threads[0] = thisthread;
-      } else {
-	ofile[0] = openfile(fileprefix, timestr, -1);
-      }
-      ithread = 0;
-      nthread = 1;
-      
-      if (ofile[0]==-1) exit(1);
-
-    } else if (splitthread) {
-      ithread = -1;
-      // Have we seen this thread this time
-      for (i=0;i<nthread;i++) {
-	if (threads[i] == thisthread) {
-	  ithread = i;
-	  break;
-	}
-      }
-      if (ithread==-1) { // Not found
-	if (nthread>=MAXTHREAD) {
-	  fprintf(stderr, "Too many threads = increase MAXTHREAD (%d). Quitting\n", MAXTHREAD);
-	  exit(1);
-	}
-	ofile[nthread] = openfile(fileprefix, timestr, thisthread);
-	if (ofile[nthread]==-1) exit(1);
-	ithread = nthread;
-	threads[nthread] = thisthread;
-	nthread++;
-      }
-    }
-    if (scale>0) {
+    if (scale>0) { // Should check nbits==16
       s16 = cdata;
       s8 = (int8_t*)cdata;
       int i;
@@ -457,7 +463,45 @@ int main (int argc, char * const argv[]) {
       nwrite = nread - padding;
     }
     
-    nwrote = write(ofile[ithread], buf, nwrite);
+    t2 = tim();
+    if (first || t2-filetime>filesize) {
+      if (first) {
+	filetime = t0;
+	first = 0;
+      } else {
+	while (t2-filetime>filesize) {
+	  filetime += filesize;
+	}
+	if (splitthread) {
+	  for (i=0;i<nthread;i++) {
+	    close(ofile[i]);
+	    ofile[i] = -1;
+          }
+	} else {
+	  close(ofile[0]);
+	  ofile[0] = -1;
+	}
+      }
+      time_t itime = (time_t)floor(filetime);
+      struct tm *date = gmtime(&itime); 
+
+      strftime(timestr, MAXSTR-1, "%j_%H%M%S", date);
+
+      if (splitthread) {
+	ofile[threadIndex] = openfile(fileprefix, timestr, thisthread);
+      } else {
+	ofile[0] = openfile(fileprefix, timestr, -1);
+      }
+      if (ofile[threadIndex]==-1) exit(1);
+
+    } else if (splitthread) {
+      if (ofile[threadIndex]==-1) {
+	ofile[threadIndex] = openfile(fileprefix, timestr, thisthread);
+	if (ofile[threadIndex]==-1) exit(1);
+      }
+    }
+    
+    nwrote = write(ofile[threadIndex], buf, nwrite);
     if (nwrote==-1) {
       perror("Error writing outfile");
       for (i=0;i<nthread;i++) close(ofile[i]);
@@ -529,32 +573,56 @@ double tim(void) {
 }
 
 void alarm_signal (int sig) {
+  int i;
   long avgpkt;
   float delta, rate, percent;
+  datastats sumstats;
+  char dropStr[5];
   t2 = tim();
   delta = t2-t1;
 
-  if (npacket==0) {
+  initstats(&sumstats);
+  for (i=0; i<nthread; i++) {
+    sumstats.npacket += allstats[i].npacket;
+    sumstats.pkt_drop += allstats[i].pkt_drop;
+    sumstats.pkt_oo += allstats[i].pkt_oo;
+    sumstats.sum_pkts += allstats[i].sum_pkts;
+    if (allstats[i].minpkt_size < sumstats.minpkt_size)
+      sumstats.minpkt_size = allstats[i].minpkt_size;
+    if (allstats[i].maxpkt_size < sumstats.maxpkt_size)
+      sumstats.maxpkt_size = allstats[i].maxpkt_size;
+  }
+
+  if (sumstats.npacket==0) {
     avgpkt = 0;
-    minpkt_size = 0;
+    sumstats.minpkt_size = 0;
   } else 
-    avgpkt = sum_pkts/npacket;
-  rate = sum_pkts*8/delta/1e6;
+    avgpkt = sumstats.sum_pkts/sumstats.npacket;
+  rate = sumstats.sum_pkts*8/delta/1e6;
 
   if (lines % UPDATE ==0) {
-    printf("  npkt   min  max  avg sec Rate Mbps  drop  %%    oo skipped\n");
+    printf("  npkt   min  max  avg sec Rate Mbps  drop  %%    oo\n");
   }
   lines++;
 
-  if (npacket==0)
+  if (sumstats.npacket==0)
     percent = 0; 
   else 
-    percent = (double)pkt_drop/(npacket+pkt_drop)*100;
+    percent = (double)sumstats.pkt_drop/(sumstats.npacket+sumstats.pkt_drop)*100;
 
-  printf("%6" PRIu64"  %4lu %4lu %4ld %3.1f  %7.3f %5lu %5.2f %3lu  %2.0f%%\n", 
-	 npacket, minpkt_size, maxpkt_size, avgpkt, delta, rate, 
-	 pkt_drop, percent, pkt_oo, skipped/(float)npacket*100.0);	
-  INIT_STATS();
+  if (1 || sumstats.pkt_drop<10000) {
+    snprintf(dropStr, 5, "%" PRIu64, sumstats.pkt_drop);
+  } else {
+    strcpy(dropStr, "XXXX");
+  }
+
+  printf("%6llu  %4d %4d %4ld %3.1f %8.3f %4s %6.2f %3" PRIu64 "\n", 
+	 (unsigned long long)sumstats.npacket, sumstats.minpkt_size, sumstats.maxpkt_size, avgpkt,
+	 delta, rate,  dropStr, percent, sumstats.pkt_oo);	
+
+  for (i=0; i<nthread; i++) 
+    initstats(&allstats[i]);
+
   t1 = t2;
   return;
 }  
@@ -574,4 +642,13 @@ int openfile(char *fileprefix, char *timestr, int threadid) {
     perror(msg);
   }
   return ofile;
+}
+
+int matchthread(datastats *allstats, int nthread, int threadID) {
+  int i;
+  for (i=0; i<nthread; i++) {
+    if (allstats[i].threadid==threadID)
+      return(i);
+  }
+  return(-1);
 }

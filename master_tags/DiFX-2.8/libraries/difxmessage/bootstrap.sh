485445d03b17b5bc51c791b9387affb88bc7a0f0
walterfb <wbrisken@gmail.com>
2022-11-04 20:03:35 +0000

    Branching DiFX-2.8
    git-svn-id: https://svn.atnf.csiro.au/difx@10742 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ master_tags/DiFX-2.8/libraries/difxmessage/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Running the GNU autotools"
+
+
+if [ -z $DIFXROOT ]; then
+    echo "\$DIFXROOT not set. Please first source setup file"
+    exit 1
+fi
+
+aclocal
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    glibtoolize --copy --force
+else
+    libtoolize --copy --force
+fi
+autoconf
+autoheader
+automake -a
+
+./configure --prefix=$DIFXROOT
+
+echo
+echo "Bootstrap should be complete. If no errors, now run make"
+echo
+

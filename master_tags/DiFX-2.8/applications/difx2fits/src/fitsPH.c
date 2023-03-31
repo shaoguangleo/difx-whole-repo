065948f2e8f3a31bc9c73d3ad1d897ece9174ee3
walterfb <wbrisken@gmail.com>
2023-03-31 02:30:10 +0000

    Complete previous commit with changelog and version bump
    git-svn-id: https://svn.atnf.csiro.au/difx@10950 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- master_tags/DiFX-2.8/applications/difx2fits/src/fitsPH.c
+++ master_tags/DiFX-2.8/applications/difx2fits/src/fitsPH.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2021 by Walter Brisken & John Morgan & Leonid Petrov *
+ *   Copyright (C) 2008-2023 by Walter Brisken & John Morgan & Leonid Petrov *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
